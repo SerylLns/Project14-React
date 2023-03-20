c38ca2feee0371c5c37a33f2cb8214b42efade7b
seryl lounis <seryllounis@gmail.com>
2023-03-20 17:43:07 +0100

    add basename to router

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       {/* basename={process.env.PUBLIC_URL} */}
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
